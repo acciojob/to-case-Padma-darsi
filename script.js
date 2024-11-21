e031c58ad085e5491721b44340e83754f5a96270
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-11-21 17:12:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function toCase(text) {
-  // write your code here
+  // Check for an empty string
+  if (text === "") {
+    return "-";
+  }
+  // Convert the string to lowercase and uppercase, then join with a "-"
+  return text.toLowerCase() + "-" + text.toUpperCase();
 }
 
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
