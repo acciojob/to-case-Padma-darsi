937785595f56927acc9ef41175bcdaa6a1c893aa
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-07-27 22:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toCase(text) {
   // write your code here
+	 // If the string is empty, return just the hyphen
+  if (text === "") return "-";
+  
+  // Convert to lowercase and uppercase, and join with "-"
+  return text.toLowerCase() + "-" + text.toUpperCase();
 }
 
 // DO not change the code below
